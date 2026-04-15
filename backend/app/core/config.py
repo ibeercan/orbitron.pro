6092cd7c616bf4ee4443ba074bc6e4f13f29c70e
Orbitron Team <orbitron@team.com>
2026-04-15 15:11:44 +0300

    Add logger export to app.core.config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,5 +1,29 @@
 from pydantic_settings import BaseSettings
 from typing import List, Optional
+import structlog
+import logging
+
+log_level = getattr(logging, "INFO", logging.INFO)
+logging.basicConfig(level=log_level)
+
+structlog.configure(
+    processors=[
+        structlog.stdlib.filter_by_level,
+        structlog.stdlib.add_logger_name,
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.format_exc_info,
+        structlog.processors.UnicodeDecoder(),
+        structlog.processors.JSONRenderer()
+    ],
+    context_class=dict,
+    logger_factory=structlog.stdlib.LoggerFactory(),
+    wrapper_class=structlog.stdlib.BoundLogger,
+    cache_logger_on_first_use=True,
+)
+
+logger = structlog.get_logger()
+
 
 class Settings(BaseSettings):
     # Database
